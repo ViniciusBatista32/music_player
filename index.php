34b76d731fdedebfd8f4ca38d251ee18f1e2f4fe
ViniciusBatista32 <viniaraujo417@gmail.com>
2023-10-06 01:28:48 -0300

    estrutura inicial

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Player</title>
+
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+    <body>
+        <div class="application">
+            <!-- TOP BAR -->
+            <div class="card top_bar">
+                <!-- VOID -->
+                <div>
+
+                </div>
+
+                <!-- SEARCH -->
+                <div>
+
+                </div>
+                
+                <!-- SETTINGS / UPLOAD / PROFILE -->
+                <div>
+
+                </div>
+            </div>
+            
+            <!-- CONTENT -->
+            <div class="content">
+                <!-- LEFT BAR -->
+                <div class="card left_bar">
+
+                </div>
+
+                <!-- NAVIGATION -->
+                <div class="navigation">
+
+                </div>
+
+                <!-- RIGHT BAR -->
+                <div class="right_bar">
+                    <!-- QUEUE -->
+                    <div class="card queue">
+                        <div class="queue_title">
+                            <p class="title_1">Fila</p>
+                        </div>
+
+                        <!-- QUEUE LIST -->
+                        <div class="queue_list">
+
+                        </div>
+                    </div>
+                    
+                    <!-- THUMB -->
+                    <div class="card thumb">
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- BOTTOM BAR -->
+            <div class="card bottom_bar">
+                <!-- VOLUME -->
+                <div>
+
+                </div>
+
+                <!-- PROGRESS CONTROLLER -->
+                <div>
+                    <!-- BUTTONS -->
+                    <div>
+
+                    </div>
+
+                    <!-- DATA -->
+                    <div>
+                        <!-- ELAPSED -->
+                        <div>
+
+                        </div>
+
+                        <!-- BAR -->
+                        <div>
+                            
+                        </div>
+
+                        <!-- REMAINING -->
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <!-- MUSIC DATA -->
+                <div>
+                    <!-- TITLE -->
+                    <div>
+                        
+                    </div>
+                    
+                    <!-- ARTIST -->
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
